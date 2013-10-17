e40439d5289c9dfe7e182bf7edb0ba737498a4ef
qingzhizhu <qingzhizhu@163.com>
2013-10-17 22:51:06 +0800

    iostream

--- test/basic/BasicTest.cpp
+++ test/basic/BasicTest.cpp
@@ -1,5 +1,8 @@
 #include "BasicTest.h"
-#include <iostream>
+#include <iostream> //cout, ci endl, string
+#include <stdio.h>  //getchat 不报错了
+#include <stdlib.h> //malloc,realloc,free 不报错了
+#include <string.h> //memset, strlen
 
 using namespace std;
 
@@ -7,6 +10,7 @@ BasicTest::BasicTest()
 {
     //ctor
     testIO();
+    testMem();
 }
 
 BasicTest::~BasicTest()
@@ -17,6 +21,16 @@ BasicTest::~BasicTest()
 
 void BasicTest::testIO()
 {
+    cout << "--------------BasicTest::testIO() 基础io----------------" << endl;
+
+    cout << "NULL | " << NULL << endl;
+
+    int *p;
+    *p = 120;
+    cout << "Pointer int | " << p << endl;
+    cout << "Pointer int address | " << &p << endl;
+    cout << "Pointer int value | " << *p << endl;
+
     char str[32] = "this is a char";
     cout << "char | " << str << endl;
     string str2 = "this is a string";
@@ -41,11 +55,55 @@ void BasicTest::testIO()
     cin >> v1 >> v2;
     cout << "The sum of " << v1 << " and " << v2
          << " is " << v1 + v2 << endl;*/
+
+    cout << "下面的cin>>num 注释了。" << endl;
+    /*
     int sum = 0, num = 0;
     while(cin>>num){
         sum += num;
     }
     cout << "sums is " << sum << endl;
-
+    */
 
 }
+
+void BasicTest::testMem()
+{
+    cout << "--------------BasicTest::testMem() 内存基本操作----------------" << endl;
+    char *cp;
+    cout << "分配长度为%d的内存块" << 100 << endl;
+	cp=(char *)malloc(100);
+	if(cp)
+		cout << "malloc Memory Allocated at: %x" << cp << endl;
+    else
+        cout << ("Not Enough Memory!\n") << endl;
+
+    int *p;
+	p=(int*)calloc(150,sizeof(char));
+	if(p)
+		cout << "calloc Memory Allocated at: %x" << p << endl;
+    else
+        cout << ("Not Enough Memory!\n") << endl;
+    //getchar();
+	p=(int*)realloc(p,100);
+    if(p)
+		cout << "realloc Memory Reallocated at: %x" << p << endl;
+    else
+        cout << ("Not Enough Memory!\n") << endl;
+	free(p);
+    //getchar();
+    cout << "realloc(NULL pointer, num) num > 0 相当于malloc()函数；" << endl;
+    cout << "realloc(pointer ,0) pointer != NULL, 相当于free()函数。" << endl;
+
+    cout << "memset 函数解释：将s中前n个字节 （typedef unsigned int size_t）用 ch 替换并返回 s 。" << endl
+         << "memset：作用是在一段内存块中填充某个给定的值，它是对较大的结构体或数组进行清零操作的一种最快方法。"
+         << endl;
+    cout << "快速给数组元素赋值为NULL， memset(m_pItems, 0, sizeof(m_pItems)); 代替重复的m_pItems[i] = NULL 操作" << endl;
+    char buffer[] = "Hello world\n";
+    cout << "Buffer before memset: " << buffer << endl;
+    memset(buffer,'*',strlen(buffer));
+    cout <<"Buffer after memset: " << buffer << endl;
+
+}
+
+
--- test/basic/BasicTest.h
+++ test/basic/BasicTest.h
@@ -12,6 +12,7 @@ class BasicTest
 
         void testIO();
 
+        void testMem();
 
     protected:
     private:
